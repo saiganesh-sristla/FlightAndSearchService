74dea3a512d9008cc08486adba8b00e8c9ac90a2
saiganesh-sristla <saiganesh3108@gmail.com>
2023-10-03 21:11:12 +0530

    city repository completed

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,28 +1,55 @@
-const {City} = require("../models/index");
+const { City } = require("../models/index");
 
 class CityRepository {
-    async createCity({name}){
-        try{
-            const city = await City.create({ name });
-            return city;
-        }
-        catch(error){
-            console.log("error occured while creating a city");
-        }
+  async createCity({ name }) {
+    try {
+      const city = await City.create({ name });
+      return city;
+    } catch (error) {
+      console.log("error occured while creating a city");
+      throw {error};
     }
+  }
 
-    async deleteCity(CityId){
-        try{
-            await City.destroy({
-                where: {
-                    id : CityId
-                }
-            })
-        }
-        catch(error){
-            console.log("error occured while deleting city");
-        }
+  async deleteCity(CityId) {
+    try {
+      await City.destroy({
+        where: {
+          id: CityId,
+        },
+      });
+    } catch (error) {
+      console.log("error occured while deleting city");
+      throw {error};
     }
+  }
+
+  async updateCity(CityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: CityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      console.log("error occured while updating data");
+      throw {error};
+    }
+  }
+
+  async getCity(CityId) {
+    try {
+      const city = await City.create({
+        where: {
+          id: CityId,
+        },
+      });
+    } catch (error) {
+      console.log("error occured while reading city");
+      throw {error};
+    }
+  }
 }
 
 module.exports = CityRepository;
